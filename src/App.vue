fa44c598ebb4152ef9e9e20a8ee5e04d1550e1c6
nicolocamassa <camassanicolo03@gmail.com>
2023-12-21 15:23:06 +0100

    [ESERCIZIO FINITO] component imported in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
     <script>
-   
+    import HelloWorld from './components/HelloWorld.vue';
     export default {
-
+      components:{
+        HelloWorld
+      }
     }
     </script>
 <template lang="">
   <div>
-    
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 <style lang="">
